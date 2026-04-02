5836bcb37104da6ea5c9de54a38d467155eca081
Leonardo <speed.leo593@gmail.com>
2026-04-02 20:25:04 +0100

    Futuro page and logic done, some minor bugs

--- futuroInput.js
+++ futuroInput.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 const inputDate = document.getElementById("date");
-const dataInicio = new Date('2024-04-04T01:00:00');
+const dateBeggining = new Date('2024-04-04T01:00:00');
+const msg = document.getElementById("msg");
 
 
 
@@ -14,6 +15,7 @@ function slash (digits, input) {
 
 function validate(digits, input) {
     if (digits.length < 8) {
+        msg.textContent = ""
         return;
     }
     const dd = digits.slice(0, 2)
@@ -23,14 +25,34 @@ function validate(digits, input) {
     // Confusing logic, tests if user inputed date is before 04/04/2026
     if (dd > 31 || mm > 12 || yyyy < 2026 || yyyy == 2026 && mm < 4 || yyyy == 2026 && mm == 4 && dd < 4) {
             input.value = ""
+            msg.textContent = ""
             console.log(digits.length)
             return;
     }
 
     const userDate = new Date(yyyy, mm - 1, dd)
-    console.log(userDate)
-    return userDate;
+    let dias = userDate.getDate() - dateBeggining.getDate();
+    let meses = userDate.getMonth() - dateBeggining.getMonth();
+    let anos = userDate.getFullYear() - dateBeggining.getFullYear();
+
+    if (dias < 0) {
+        meses --
+        const mesAnterior = new Date(userDate.getFullYear(), userDate.getMonth(), 0);
+        dias += mesAnterior.getDate();
+      }
+
+      if (meses < 0) {
+        anos--;
+        meses += 12
+      }
+
+    
+    console.log(dias, meses, anos);
+    msg.textContent = `Estaremos juntos há ${anos} anos, ${meses} meses, ${dias} dias`;
+    
 }
+
+
 
 inputDate.addEventListener("keydown", (e) => {
     if (!/^\d$/.test(e.key) && !['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight', 'Tab'].includes(e.key)) {
